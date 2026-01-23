43dc94d1595a6901cb797a17245edffb86c39159
Faisal <softtechrana05@gmail.com>
2026-01-24 00:14:13 +0500

    OOP_Crud

---
+++ database.php
@@ -0,0 +1,51 @@
+<?php
+class Database{
+    private $db_host = "127.0.0.1";
+    private $db_user = "root";
+    private $db_pass = "";
+    private $db_name = "testing";
+    private $mysqli = "";
+    private $result = array();
+    private $conn = false;
+// Where Connection is being made ?
+    public function __construct(){
+        if(!$this->conn){ // Only to store Error.
+        $this->mysqli = new mysqli($this->db_host, $this->db_user, $this->db_pass, $this->db_name);
+            if($this->mysqli->connect_error){
+            array_push($this->result, $this->mysqli->connect_error);
+            return false; // return false as Connnection Closed/failed.
+        }
+        else{
+            return true; // return true as connection made.
+        }
+        }
+    }
+    public function insert(){
+
+    }
+    public function update(){
+
+    }
+    public function delete(){
+
+
+    }
+    public function select(){
+
+    }
+    public function __destruct(){
+    if($this->conn){
+        if($this->mysqli->close()){
+            $this->conn = false; // 
+            return true; // return if Condition as close connection true.
+        }
+    }
+    else{
+        return false; // otherwise return false as aleady close connection.
+    }
+    }
+}
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+require "database.php";
+$obj = new Database();
+
+
+?>
